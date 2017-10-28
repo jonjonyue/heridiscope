0401ccb756bd39553608db11702f5143ed3e74ba
Jonathan Yue <yuejonathan@wustl.edu>
2017-10-27 23:57:04 -0500

    Adding object oriented stuff

---
+++ Herediscope/Phenotypes.swift
@@ -0,0 +1,17 @@
+//
+//  Phenotypes.swift
+//  Herediscope
+//
+//  Created by Jonathan yue on 10/27/17.
+//  Copyright © 2017 Genesis. All rights reserved.
+//
+
+import Foundation
+
+struct Phenotype {
+    var name: String
+    var description: String
+    var result: Result
+    var questions: [Question]
+    var answered: Bool
+}
