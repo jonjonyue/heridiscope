0d7752ce38a884621d96c62d317570622513ed1e
Taehoon Bang <taehoonbang@taehoons-mbp.dhcp.wustl.edu>
2017-10-28 10:16:51 -0500

    Bar Color, Bar Title Color (changed in AppDelegate.swift), and icons

--- Herediscope/ChildViewController.swift
+++ Herediscope/ChildViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChildViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
--- Herediscope/YourViewController.swift
+++ Herediscope/YourViewController.swift
@@ -12,6 +12,7 @@ class YourViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
